97dcc0b3a0ec31397b8d33e41cd0487057528f96
dinies <edoardo.ghini@live.it>
2020-12-15 17:27:08 +0000

    reordered script instructions

--- install_lunar_imgui_xenial.sh
+++ install_lunar_imgui_xenial.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 set -e
 
+git config --global user.name "dinies"
+git config --global user.email edoardo.ghini@live.it
+mkdir -p ~/catkin_ws/src
+
+#cloning repositories
+git clone https://github.com/dinies/dotfiles.git
+git clone https://github.com/opencv/opencv.git
+git clone  https://github.com/nlohmann/json.git
+git clone https://github.com/dinies/glad.git
+git clone https://github.com/glfw/glfw.git
+cd ~/catkin_ws/src/
+git clone https://gitlab.inria.fr/telemovtop/teletop_gui.git
+git checkout dev
+
 #installing utilities
 sudo apt update
 sudo apt install -y vim tmux gdb silversearcher-ag tree
 
-cd ~
-git clone https://github.com/dinies/dotfiles.git
-cd dotfiles/scripts
+
+
+#setting-up custom dotfiles
+cd ~/dotfiles/scripts
 ./set_up.sh
-
-
 echo "alias cm='catkin_make'" >> ~/.bashrc
 echo "wssource() {" >> ~/.bashrc
 echo "  source \$1/devel/setup.bash" >> ~/.bashrc
 echo "}" >> ~/.bashrc
-
-git config --global user.name "dinies"
-git config --global user.email edoardo.ghini@live.it
 
 
 #installing ROS lunar 
@@ -30,10 +40,8 @@ source /opt/ros/lunar/setup.bash
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
-mkdir -p ~/catkin_ws/src
 
 #installing opencv
-cd ~
 sudo apt-get install -y gcc g++
 sudo apt-get install -y python-dev python-numpy
 sudo apt-get install -y python3-dev python3-numpy
@@ -44,36 +52,31 @@ sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y libpng-dev
 sudo apt-get install -y libjpeg-dev
 sudo apt-get install -y libopenexr-dev
-git clone https://github.com/opencv/opencv.git
+
 cd ~/opencv/
 git fetch
 git fetch --tags
 git checkout 3.1.0
 mkdir build && cd build
 cmake -DWITH_CUDA:BOOL="0" ..
-make -j
-sudo make install
-
+make -j && sudo make install
 
 #installing nlohmann_json.
-cd ~
-git clone  https://github.com/nlohmann/json.git
-cd json/
+cd ~/json/
 mkdir build && cd build
 cmake ..
-make -j
-sudo make install
+make -j && sudo make install
 
 
 #installing glad glfw
-cd ~
-git clone https://github.com/dinies/glad.git
-git clone https://github.com/glfw/glfw.git
-cd glfw/
+cd ~/glfw/
 mkdir build && cd build/
 cmake ..
-make -j4
-sudo make install
+make -j && sudo make install
 
-cd ~/catkin_ws/src/
-git clone https://gitlab.inria.fr/telemovtop/teletop_gui.git
+#clean-up
+rm -rf ~/opencv
+rm -rf ~/json
+rm -rf ~/glfw
+
+
